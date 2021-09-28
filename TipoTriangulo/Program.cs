1550683c9296925364ad6d19776545c2d3ed38eb
vinicioslop <vinicioslop7@hotmail.com>
2021-09-28 15:22:48 -0300

    Implementa solução do desafio de tipo de triangulo

--- TipoTriangulo/Program.cs
+++ TipoTriangulo/Program.cs
@@ -6,70 +6,38 @@ namespace TipoTriangulo
     {
         static void Main(string[] args)
         {
-            string[] s = Console.ReadLine().Split(' ');
-            double a = double.Parse(s[0]);
-            double b = double.Parse(s[1]);
-            double c = double.Parse(s[2]);
+            string[] line = Console.ReadLine().Split(" ");
+            double A = double.Parse(line[0]);
+            double B = double.Parse(line[1]);
+            double C = double.Parse(line[2]);
+            double aux;
 
-            double[] lado = new double[3];
-
-            if (a > b)
-                if (b > c)
-                {
-                    lado[0] = a;
-                    lado[1] = b;
-                    lado[2] = c;
-                }
-                // a>b>c
-                else if (a > c)
-                {
-                    lado[0] = a;
-                    lado[1] = c;
-                    lado[2] = b;
-                }
-                else
-                {
-                    lado[0] = c;
-                    lado[1] = a;
-                    lado[2] = b;
-                }
-            else if (b > c)
+            if (B > A && B > C)
             {
-                if (a > c)
-                {
-                    lado[0] = b;
-                    lado[1] = a;
-                    lado[2] = c;
-                }
-                else
-                {
-                    lado[0] = b;
-                    lado[1] = c;
-                    lado[2] = a;
-                }
+                aux = A;
+                A = B;
+                B = aux;
+            }
+            else if (C > A)
+            {
+                aux = A;
+                A = C;
+                C = aux;
+            }
+
+            if (A >= (B + C))
+            {
+                Console.WriteLine("NAO FORMA TRIANGULO");
             }
             else
             {
-                lado[0] = c;
-                lado[1] = b;
-                lado[2] = a;
+                if (A * A == (B * B) + (C * C)) Console.WriteLine("TRIANGULO RETANGULO");
+                if (A * A > (B * B) + (C * C)) Console.WriteLine("TRIANGULO OBTUSANGULO");
+                if (A * A < (B * B) + (C * C)) Console.WriteLine("TRIANGULO ACUTANGULO");
+                if (A == B && A == C) Console.WriteLine("TRIANGULO EQUILATERO");
+                if ((A == B && A != C) || (A == C && A != B) || (B == C && B != A))
+                    Console.WriteLine("TRIANGULO ISOSCELES");
             }
-
-            //continue a solucao
-            if (!(a + b > c && a + c > b && b + c > a))
-                Console.WriteLine("NAO FORMA TRIANGULO");
-            else if (Math.Pow(lado[0], 2) == (Math.Pow(lado[1], 2) + Math.Pow(lado[2], 2)))
-                Console.WriteLine("TRIANGULO RETANGULO");
-            else if (Math.Pow(lado[0], 2) < (Math.Pow(lado[1], 2) + Math.Pow(lado[2], 2)))
-                Console.WriteLine("TRIANGULO OBTUSANGULO");
-            else if (Math.Pow(lado[0], 2) > (Math.Pow(lado[1], 2) + Math.Pow(lado[2], 2)))
-                Console.WriteLine("TRIANGULO ACUTANGULO");
-            if (lado[0] == lado[1] && lado[0] == lado[2] && lado[1] == lado[2])
-                Console.WriteLine("TRIANGULO EQUILATERO");
-            if (lado[0] == lado[1] || lado[0] == lado[2] || lado[2] == lado[1])
-                Console.WriteLine("TRIANGULO ISOSCELES");
-
-            Console.ReadLine();
         }
     }
 }
